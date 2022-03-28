f5ab567cfa2bc9cd5b5a9a16cc95e82796bed7a7
williamcbarker <62165813+williamcbarker@users.noreply.github.com>
2022-03-27 22:59:56 -0400

    Add files via upload

---
+++ williambarkerweek2.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print('Hello World! I wonder why that is always the default coding text to start with')
+
+y = 2+2
+print(y)
+
+x = 2-1
+print(x)
+
+z = 2*2
+print(z)
+
+w = 4/2
+print(w)
+
+string1 = "Cameron is "
+string2 = "super cool!"
+string3 = string1 + string2
+print(string3)
+
+list1 = ['Cars', 'Trucks', 'Bikes', 'PLanes']
+print(list1)
+
+list1.append('Trains')
+print(list1)
+
+tuple1 = (1, 3, 5, 7)
+print(tuple1)
